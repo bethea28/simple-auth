dd492e60bd291df04dce6cabea85c803a3cd5de1
Bryan Bethea <bryanbethea28@gmail.com>
2022-03-12 01:38:40 -0500

    links

--- data.js
+++ data.js
@@ -1,3 +1,7 @@
+/*
+https://javascript.plainenglish.io/simple-authentication-and-authorization-in-graphql-5293c0458fc
+https://daily.dev/blog/authentication-and-authorization-in-graphql
+*/
 const books = [
   {
     title: 'Harry Potter and the Chamber of Secrets',
